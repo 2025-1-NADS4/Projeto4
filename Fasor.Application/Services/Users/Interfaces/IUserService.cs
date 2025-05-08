a4aeca43c463625021d5255815c9a72d5278b891
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-08 17:20:37 -0300

    create flow User in Service layer

--- Fasor.Application/Services/Users/Interfaces/IUserService.cs
+++ Fasor.Application/Services/Users/Interfaces/IUserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ErrorOr;
+using Fasor.Domain.Aggregates;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace Fasor.Application.Services.Users.Interfaces
 {
-    internal interface IUserService
+    public interface IUserService
     {
+        Task<ErrorOr<User>> GetUserByIdAsync(Guid id);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> CreateUser(string name, string surname, string cpf, string email, DateTime dateBirth, IEnumerable<Company>? preferences);
+        
     }
 }
--- Fasor.Application/Services/Users/UserService.cs
+++ Fasor.Application/Services/Users/UserService.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ErrorOr;
+using Fasor.Application.Services.Users.Interfaces;
+using Fasor.Domain.Aggregates;
+using Fasor.Infrastructure.Repositories.Users.Interfaces;
 
 namespace Fasor.Application.Services.Users
 {
-    internal class UserService
+    public class UserService(IUserRepository userRepository) : IUserService
     {
+        public async Task<ErrorOr<User>> GetUserByIdAsync(Guid id)
+        {
+            var result = await userRepository.GetByIdAsync(id);
+            if (result.IsError) return result.Errors;
+
+            var user = result.Value;
+
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            var users = await userRepository.GetAllAsync();
+            return users;
+        }
+
+        public async Task<User> CreateUser(string name, string surname, string cpf, string email, DateTime dateBirth, IEnumerable<Company>? preferences)
+        {
+            var result = await userRepository.CreateUser(name, surname, cpf, email, dateBirth, preferences);
+
+            return result;
+        }
     }
 }
