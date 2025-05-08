97fb38b01739ffd3ee7fce18981c399bb561b130
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-08 17:19:40 -0300

    Create flow RideQuote in layer service

---
+++ Fasor.Application/Services/RideOptions/Interfaces/IRideOptionService.cs
@@ -0,0 +1,15 @@
+﻿using Fasor.Domain.Aggregates;
+
+namespace Fasor.Application.Services.RideOptions.Interfaces
+{
+    public interface IRideOptionService
+    {
+        Task<RideOption> CreateRideOptionAsync(CompanyAppService companyService, DateTime timeRide,
+            RideQuote rideQuote, DateTime estimatedTime, string urlRedirect, decimal price);
+
+        Task<IEnumerable<RideOption>> GetAllRideOptionsByQuoteAsync(Guid idQuote);
+
+        Task<RideOption> GetRideOptionByIdAsync(Guid idRideOption);
+        Task<bool> DeleteRideOptionByIdAsync(Guid idRideOption);
+    }
+}
