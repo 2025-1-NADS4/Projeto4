5ded9211634fb7ecbb232ec5e4db046c984323a9
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-08 17:22:21 -0300

    create interface user repository

--- Fasor.Infrastructure/Repositories/Users/Interfaces/IUserRepository.cs
+++ Fasor.Infrastructure/Repositories/Users/Interfaces/IUserRepository.cs
@@ -1,11 +1,12 @@
-﻿using Fasor.Domain.Aggregates;
+﻿using ErrorOr;
+using Fasor.Domain.Aggregates;
 using Fasor.Infrastructure.Repositories.Users.Dtos;
 
 namespace Fasor.Infrastructure.Repositories.Users.Interfaces
 {
     public interface IUserRepository
     {
-        Task<User?> GetByIdAsync(Guid id);
+        Task<ErrorOr<User?>> GetByIdAsync(Guid id);
         Task<IEnumerable<User>> GetAllAsync();
         Task<User> CreateUser(string name, string surname, string cpf, string email, DateTime dateBirth, IEnumerable<Company>? preferences);
         Task<bool> UpdateUserAsync(Guid id, UpdateUserDto dto);
