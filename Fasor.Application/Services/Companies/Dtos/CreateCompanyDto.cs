68e2eba3805d4dd820c7d59471b54430d509bef3
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-08 17:16:56 -0300

    Create Dto Layer Service of Company

---
+++ Fasor.Application/Services/Companies/Dtos/CreateCompanyDto.cs
@@ -0,0 +1,21 @@
+﻿using Fasor.Domain.Aggregates;
+
+namespace Fasor.Application.Services.Companies.Dtos
+{
+    public class CreateCompanyDto
+    {
+        public string TradeName { get; set; }
+        public string Cnpj { get; set; }
+        public List<CompanyAppService>? CompanyAppService { get; set; }
+
+        public static explicit operator CreateCompanyDto(Company domain)
+        {
+            return new CreateCompanyDto
+            {
+                TradeName = domain.TradeName,
+                Cnpj = domain.Cnpj,
+                CompanyAppService = domain.CompanyAppServices
+            };
+        }
+    }
+}
---
+++ Fasor.Application/Services/Companies/Dtos/UpdateCompanyDto.cs
@@ -0,0 +1,21 @@
+﻿using Fasor.Domain.Aggregates;
+
+namespace Fasor.Application.Services.Companies.Dtos
+{
+    public class UpdateCompanyDto
+    {
+        public string TradeName { get; set; }
+        public string Cnpj { get; set; }
+        public List<CompanyAppService>? CompanyAppService { get; set; }
+
+        public static explicit operator UpdateCompanyDto(Company domain)
+        {
+            return new UpdateCompanyDto
+            {
+                TradeName = domain.TradeName,
+                Cnpj = domain.Cnpj,
+                CompanyAppService = domain.CompanyAppServices
+            };
+        }
+    }
+}
