c02a4fb4df8d457449594034cc455789a0041ff2
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-08 17:18:16 -0300

    Create Flow Service of CompanyAppServices

---
+++ Fasor.Application/Services/CompanyAppServices/Interfaces/ICompanyAppServicesService.cs
@@ -0,0 +1,15 @@
+﻿using Fasor.Domain.Aggregates;
+
+namespace Fasor.Application.Services.CompanyServices.Interfaces
+{
+    public interface ICompanyAppServicesService
+    {
+        Task<IEnumerable<CompanyAppService>> GetAllCompanyAppServicesAsync();
+        Task<CompanyAppService> GetCompanyAppServiceByIdAsync(Guid id);
+        Task<CompanyAppService> CreateCompanyAppServiceAsync(Guid idCompany, string NameService);
+        Task<bool> DeleteCompanyAppServiceAsync(Guid id);
+        Task<CompanyAppService> UpdateCompanyAppServiceAsync(Guid idCompanyAppService);
+        Task<CompanyAppService> CompanyAppServiceInactiveAsync(Guid id);
+        Task<CompanyAppService> CompanyAppServiceActiveAsync(Guid id);
+    }
+}
