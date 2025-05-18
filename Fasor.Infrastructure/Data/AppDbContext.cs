ded534fddf59c2775d34e4c0c5d54c9eb4742155
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-18 02:24:04 -0300

    reconfiguring DbContext for migrations

--- Fasor.Infrastructure/Data/AppDbContext.cs
+++ Fasor.Infrastructure/Data/AppDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Fasor.Domain.Aggregates;
 using Microsoft.EntityFrameworkCore;
 
-namespace Fasor.Infrastructure.Aggregates.Data
+namespace Fasor.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
@@ -9,7 +9,8 @@ namespace Fasor.Infrastructure.Aggregates.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Company> Companies { get; set; }
-        public DbSet<CompanyAppService> CompanyServices { get; set; }
+        public DbSet<AppService> AppServices { get; set; }
+        public DbSet<CompanyRide> CompanyRides { get; set; }
         public DbSet<RideOption> RideOptions { get; set; }
         public DbSet<RideQuote> RideQuotes { get; set; }
 
@@ -17,12 +18,56 @@ namespace Fasor.Infrastructure.Aggregates.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<User>().HasKey(u => u.Id);
-            modelBuilder.Entity<Company>().HasKey(u => u.Id);
-            modelBuilder.Entity<CompanyAppService>().HasKey(u => u.Id);
-            modelBuilder.Entity<RideOption>().HasKey(u => u.Id);
-            modelBuilder.Entity<RideQuote>().HasKey(u => u.Id);
+            // User - Company (muitos para 1)
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Company)
+                .WithMany(c => c.Users)
+                .HasForeignKey(u => u.CompanyId);
 
+            // Company - CompanyCompanyRide (muitos para muitos via entidade de junção)
+            modelBuilder.Entity<CompanyCompanyRide>()
+                .HasKey(ccr => new { ccr.CompanyId, ccr.CompanyRideId });
+
+            modelBuilder.Entity<CompanyCompanyRide>()
+                .HasOne(ccr => ccr.Company)
+                .WithMany(c => c.CompanyCompanyRides)
+                .HasForeignKey(ccr => ccr.CompanyId);
+
+            modelBuilder.Entity<CompanyCompanyRide>()
+                .HasOne(ccr => ccr.CompanyRide)
+                .WithMany(cr => cr.CompanyCompanyRides)
+                .HasForeignKey(ccr => ccr.CompanyRideId);
+
+            // CompanyRide - AppService (1 para muitos)
+            modelBuilder.Entity<AppService>()
+                .HasOne(a => a.CompanyRide)
+                .WithMany(cr => cr.AppServices)
+                .HasForeignKey(a => a.CompanyRideId);
+
+            // RideOption chave primária
+            modelBuilder.Entity<RideOption>()
+                .HasKey(ro => ro.Id);
+
+            // RideQuote - User (muitos para 1)
+            modelBuilder.Entity<RideQuote>()
+                .HasOne(rq => rq.User)
+                .WithMany(u => u.RideQuotes)
+                .HasForeignKey(rq => rq.UserId);
+
+            // RideQuote - RideOptions (1 para muitos)
+            modelBuilder.Entity<RideQuote>()
+                .HasMany(rq => rq.RideOptions)
+                .WithOne(ro => ro.RideQuote)
+                .HasForeignKey(ro => ro.QuoteId);
+
+            // Company chave primária
+            modelBuilder.Entity<Company>()
+                .HasKey(c => c.Id);
+
+            // CompanyRide chave primária
+            modelBuilder.Entity<CompanyRide>()
+                .HasKey(cr => cr.Id);
         }
+
     }
 }
