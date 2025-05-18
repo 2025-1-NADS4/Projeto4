43c81e160fbb1660c767b652dd9d0f16eef59e2b
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-18 03:04:04 -0300

    create requestDto CreateRide

---
+++ Fasor.Api/Dtos/CreateRideQuoteRequest.cs
@@ -0,0 +1,15 @@
+﻿using Fasor.Domain.Aggregates;
+
+namespace Fasor.Api.Dtos
+{
+    public class CreateRideQuoteRequest
+    {
+        public string OriginAddress { get; set; }
+        public string DestinationAddress { get; set; }
+        public float LatitudeOrigin { get; set; }
+        public float LongitudeOrigin { get; set; }
+        public float LatitudeDestination { get; set; }
+        public float LongitudeDestination { get; set; }
+        public List<RideOption> RideOptions { get; set; }
+    }
+}
