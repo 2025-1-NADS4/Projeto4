6b11948ddd67a552aadc7a0f36b6dcce2e81f426
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-18 03:06:07 -0300

    create company ride errors

---
+++ Fasor.Domain/Shared/Errors/CompanyRideErrors.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+namespace Fasor.Domain.Aggregates;
+
+public static class CompanyRideErrors
+{
+    public static readonly Error TradeNameIsRequired = Error.Validation(
+        code: "CompanyRide.TradeNameRequired",
+        description: "O nome da empresa de corrida é obrigatório."
+    );
+}
