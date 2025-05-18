469475080317181d5b78a6affa4e379e48c4fe69
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-18 02:30:49 -0300

    Creating Controllers

---
+++ Fasor.Api/Controllers/RideQuotesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Fasor.Domain.Aggregates;
+using Fasor.Infrastructure.Data;
+using Fasor.Application.Services.RideQuotes.Interfaces;
+using Fasor.Api.Dtos;
+
+namespace Fasor.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RideQuotesController(IRideQuoteService _rideQuoteService) : ControllerBase
+    {
+     
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RideQuote>> GetRideQuote(Guid id)
+        {
+            var rideQuote = await _rideQuoteService.GetRideQuoteByIdAsync(id);
+
+            if (rideQuote == null)
+            {
+                return NotFound();
+            }
+
+            return rideQuote;
+        }
+        [HttpPost]
+        public async Task<ActionResult<RideQuote>> CreateRideQuote([FromBody] CreateRideQuoteRequest request)
+        {
+            var result = _rideQuoteService.CreateRideQuoteAsync(request.OriginAddress, request.DestinationAddress,
+                request.LatitudeOrigin, request.LongitudeOrigin, request.LatitudeDestination, request.LongitudeDestination, request.RideOptions);
+
+            return Ok(result);
+        }
+
+   
+
+    }
+}
---
+++ Fasor.Api/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Fasor.Application.Services.Users.Interfaces;
+using Fasor.Infrastructure.Repositories.Users.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fasor.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UpdateUserDto dto)
+        {
+            var result = await _userService.UpdateUserAsync(id, dto);
+
+            if (result.IsError)
+                return BadRequest(result.Errors);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var result = await _userService.DeleteUserAsync(id);
+
+            if (result.IsError)
+                return BadRequest(result.Errors);
+
+            return NoContent();
+        }
+    }
+}
