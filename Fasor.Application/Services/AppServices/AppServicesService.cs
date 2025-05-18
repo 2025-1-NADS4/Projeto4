12c8f5e012379440a0136f68e6d13d0bb51e00fd
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-18 03:03:37 -0300

    Adding AppServicesService

---
+++ Fasor.Application/Services/AppServices/AppServicesService.cs
@@ -0,0 +1,58 @@
+﻿using Fasor.Application.Services.CompanyServices.Interfaces;
+using Fasor.Domain.Aggregates;
+using Fasor.Infrastructure.Repositories.CompanyServices.Interfaces;
+
+namespace Fasor.Application.Services.CompanyServices
+{
+    public class AppServicesService(
+        IAppServicesRepository _AppServicesRepository
+        ) : IAppServicesService
+    {
+        public async Task<IEnumerable<AppService>> GetAllAppServicesAsync()
+        {
+            var AppServices = await _AppServicesRepository.GetAllAppServicesAsync();
+            return AppServices;
+        }
+
+        public async Task<AppService> GetAppServiceByIdAsync(Guid id)
+        {
+            var AppService = await _AppServicesRepository.GetAppServiceByIdAsync(id);
+            return AppService;
+        }
+
+        public async Task<AppService> CreateAppServiceAsync(Guid idCompanyRide, string nameService)
+        {
+            var AppService = await _AppServicesRepository.CreateAppServiceAsync(idCompanyRide, nameService);
+            return AppService;
+        }
+
+        public async Task<bool> DeleteAppServiceAsync(Guid id)
+        {
+            var result = await _AppServicesRepository.DeleteAppServiceAsync(id);
+            return result;
+        }
+
+        public async Task<AppService> UpdateAppServiceAsync(Guid idAppService)
+        {
+            var AppService = await _AppServicesRepository.GetAppServiceByIdAsync(idAppService);
+            var result = await _AppServicesRepository.UpdateAppServiceAsync(AppService);
+            return result;
+        }
+
+        public async Task<AppService> AppServiceInactiveAsync(Guid id)
+        {
+            var AppService = await _AppServicesRepository.GetAppServiceByIdAsync(id);
+            if (AppService == null) return null;
+            var result = await _AppServicesRepository.AppServiceInactiveAsync(AppService);
+            return result;
+        }
+
+        public async Task<AppService> AppServiceActiveAsync(Guid id)
+        {
+            var AppService = await _AppServicesRepository.GetAppServiceByIdAsync(id);
+            if (AppService == null) return null;
+            var result = await _AppServicesRepository.AppServiceActiveAsync(AppService);
+            return result;
+        }
+    }
+}
---
+++ Fasor.Application/Services/AppServices/Interfaces/IAppServicesService.cs
@@ -0,0 +1,15 @@
+﻿using Fasor.Domain.Aggregates;
+
+namespace Fasor.Application.Services.CompanyServices.Interfaces
+{
+    public interface IAppServicesService
+    {
+        Task<IEnumerable<AppService>> GetAllAppServicesAsync();
+        Task<AppService> GetAppServiceByIdAsync(Guid id);
+        Task<AppService> CreateAppServiceAsync(Guid idCompanyRide, string nameService);
+        Task<bool> DeleteAppServiceAsync(Guid id);
+        Task<AppService> UpdateAppServiceAsync(Guid idAppService);
+        Task<AppService> AppServiceInactiveAsync(Guid id);
+        Task<AppService> AppServiceActiveAsync(Guid id);
+    }
+}
