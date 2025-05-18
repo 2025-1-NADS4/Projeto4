7dbfe1e16394b1d2903857a451b07f2e085cfb77
giovannebraga10 <gi.braga24@hotmail.com>
2025-05-18 02:30:34 -0300

    Creating CompanyRideRepository

---
+++ Fasor.Infrastructure/Repositories/CompanyRides/CompanyRideRepository.cs
@@ -0,0 +1,72 @@
+﻿using Fasor.Infrastructure.Data;
+using Fasor.Infrastructure.Repositories.CompanyRides.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fasor.Infrastructure.Repositories.CompanyRides
+{
+    public class CompanyRideRepository(AppDbContext _context) : ICompanyRideRepository
+    {
+        public async Task<CompanyRide> GetCompanyRideByIdAsync(Guid id)
+        {
+            return await _context.CompanyRides
+                .Include(c => c.CompanyCompanyRides)
+                .Include(c => c.AppServices)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<List<CompanyRide>> GetAllAsync()
+        {
+            return await _context.CompanyRides
+                .Include(c => c.CompanyCompanyRides)
+                .Include(c => c.AppServices)
+                .ToListAsync();
+        }
+
+        public async Task<CompanyRide> CreateCompanyRideAsync(CompanyRide companyRide)
+        {
+            _context.CompanyRides.Add(companyRide);
+            await _context.SaveChangesAsync();
+            return companyRide;
+        }
+
+        public async Task<CompanyRide> UpdateCompanyRideAsync(CompanyRide companyRide)
+        {
+            _context.CompanyRides.Update(companyRide);
+            await _context.SaveChangesAsync();
+            return companyRide;
+        }
+
+        public async Task<bool> DeleteCompanyRideAsync(Guid id)
+        {
+            var entity = await _context.CompanyRides.FindAsync(id);
+            if (entity == null)
+                return false;
+
+            _context.CompanyRides.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> InactiveCompanyRideAsync(Guid id)
+        {
+            var entity = await _context.CompanyRides.FindAsync(id);
+            if (entity == null)
+                return false;
+
+            entity.Inactivate() ;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> ActiveCompanyRideAsync(Guid id)
+        {
+            var entity = await _context.CompanyRides.FindAsync(id);
+            if (entity == null)
+                return false;
+
+            entity.Activate();
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ Fasor.Infrastructure/Repositories/CompanyRides/Interface/ICompanyRideRepository.cs
@@ -0,0 +1,16 @@
+﻿using ErrorOr;
+using Fasor.Domain.Aggregates;
+
+namespace Fasor.Infrastructure.Repositories.CompanyRides.Interface
+{
+    public interface ICompanyRideRepository
+    {
+        public Task<CompanyRide> GetCompanyRideByIdAsync(Guid id);
+        public Task<List<CompanyRide>> GetAllAsync();
+        public Task<CompanyRide> CreateCompanyRideAsync(CompanyRide companyRide);
+        public Task<CompanyRide> UpdateCompanyRideAsync(CompanyRide companyRide);
+        public Task<bool> DeleteCompanyRideAsync(Guid id);
+        public Task<bool> InactiveCompanyRideAsync(Guid id);
+        public Task<bool> ActiveCompanyRideAsync(Guid id);
+    }
+}
